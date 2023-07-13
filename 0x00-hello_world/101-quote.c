529ac50d0cc6d6d42f227ae146613acafefbc85d
“richiearius” <starkyrichie@gmail.com>
2023-07-13 09:41:40 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -4,8 +4,9 @@
  * main -  program that prints to the standard error."
  * Return: 1 (BIG WIN)
  */
+
 int main(void)
 {
-	fput("and that piece of art is useful" - Dora Korpar, 2015-10-19\", stdout);
-       return (1);
+fput("and that piece of art is useful" - Dora Korpar, 2015 - 10 - 19\", stdout);
+return (1);
 }
