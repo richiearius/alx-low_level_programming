3673b899b0cf9b8b34a1c1cd083c068acbcc8c91
“richiearius” <starkyrichie@gmail.com>
2023-07-13 04:46:52 -0700

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - Puts program that prints a new line
+ * Return: 0 (BIG WIN)
+ */		
+int main(void)	
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
+
