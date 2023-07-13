4b75356a1668b04a8040646c8cfdefcce3ead014
“richiearius” <starkyrichie@gmail.com>
2023-07-13 07:04:58 -0700

    5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ *  main - Printf function that prints a new line
+ *  Return: 0 (BIG WIN)
+ */
+
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,\n");
+return (0);
+}
