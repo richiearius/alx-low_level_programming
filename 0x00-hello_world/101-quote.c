06120f33a2425503060b496aa4f405f250e73abe
“richiearius” <starkyrichie@gmail.com>
2023-07-13 09:31:25 -0700

    101-quote.c

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <unistd.h>
+/**
+ * main -  program that prints to the standard error."
+ * Return: 1 (BIG WIN)
+ */
+int main(void)
+{
+	fput("and that piece of art is useful" - Dora Korpar, 2015-10-19\", stdout);
+       return (1);
+}
