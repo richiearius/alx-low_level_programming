17a50320cf7158834a00b3391e194b5a362d362b
“richiearius” <starkyrichie@gmail.com>
2023-07-14 08:59:44 -0700

    5-print_numbers.c

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -1,11 +1,13 @@
 #include <stdio.h>
-
+/**
+ * main - program to print single numbers in base 10 starting from 0, new line
+ * Return: 0 (BIG WIN)
+ */
 int main(void)
 {
 int i;
 for (i = 0; i < 10; i++)
-{
-printf("%d\n", i);
-}
+printf("%d", i);
+putchar('\n');
 return (0);
 }
