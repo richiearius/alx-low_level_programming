1775cf6d3d356024422e0a6372bc543da58e05b0
“richiearius” <starkyrichie@gmail.com>
2023-07-14 16:15:31 -0700

    8-print_base16

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+/**
+ * main - program to print numbers of base 16 in lowercase
+ * Return: 0 (BIG wIN)
+ */
+int main(void)
+{
+	char f;
+	int k;
+	k = 0;
+	while
+		(k <= 9)
+		{
+			putchar(k + '0');
+			k++;
+		}
+	f = 'a';
+	while
+		(f <= 'a')
+		{
+			putchar(f);
+			f++;
+		}
+	return (0);
+}
