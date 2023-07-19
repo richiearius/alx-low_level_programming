11a9cb8d39430e74e536a2896eb4c267b52aca98
“richiearius” <starkyrichie@gmail.com>
2023-07-19 09:33:38 -0700

    10-add.c

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * add - adds two integers and returns the result
+ * @n1: 1st character to be checked
+ * @n2: 2nd character to be checked
+ * Return: adds two integers and returns the result
+ */
+int add(int n1, int n2)
+{
+return (n1 + n2);
+}
