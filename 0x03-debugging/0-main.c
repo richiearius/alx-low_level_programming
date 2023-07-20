2c40d55ec4e5baabd3f51211b117a89849db8d91
“richiearius” <starkyrichie@gmail.com>
2023-07-20 09:15:04 -0700

    0-main.c

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * main - test for a postitive_or_negative() function
+ * @i: character to be checked
+ * Return: 0 (BIG WIN)
+ */
+int main(void)
+{
+int i;
+i = 0;
+positive_or_negative(i);
+return (0);
+}
