2aff33088f8c03767af7ccd4f7b674620a5d94c0
“richiearius” <starkyrichie@gmail.com>
2023-07-21 04:56:59 -0700

    2-mul.c

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * mul - multiplies two integers
+ * @w: 1st character
+ * @q: 2nd character
+ * Return: answer to multiplication of w and q
+ */
+int mul(int w, int q)
+{
+return (w * q);
+}
