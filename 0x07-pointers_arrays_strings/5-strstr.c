31d374fd7795ac5f2947893ca5a59c4733190eb5
“richiearius” <starkyrichie@gmail.com>
2023-07-31 07:57:14 -0700

    5-strstr

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * _strstr - locates a substring.
+ * @haystack: input
+ * @needle: input
+ * Return: 0 (BIG WIN)
+ */
+char *_strstr(char *haystack, char *needle)
+{
+for (; *haystack != '\0'; haystack++)
+{
+char *one = haystack;
+char *two = needle;
+while (*one == *two && *two != '\0')
+{
+one++;
+two++;
+}
+if (*two == '\0')
+return (haystack);
+}
+return (NULL);
+}
