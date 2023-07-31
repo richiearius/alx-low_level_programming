ba3684bdebfef96a79317e0c3db37cf8f52ffed4
“richiearius” <starkyrichie@gmail.com>
2023-07-31 05:30:57 -0700

    3-strspn.c

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -7,13 +7,13 @@
  */
 unsigned int _strspn(char *s, char *accept)
 {
-unsigned in tn = 0;
+unsigned int n = 0;
 int r;
 while (*s)
 {
 for (r = 0; accept[r]; r++)
 {
-if (*S == accept[r])
+if (*s == accept[r])
 {
 n++;
 break;
