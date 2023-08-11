abaf60d3f8a2531feca8c430f9c0f491f0c3b9cd
“richiearius” <starkyrichie@gmail.com>
2023-08-11 02:14:07 -0700

    main.h

--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -1,8 +1,11 @@
-#ifndef _MAIN_H
-#define _MAIN_H
+#include <stdio.h>
+#include <stdlib.h>
+#include <limits.h>
 void *malloc_checked(unsigned int b);
 char *string_nconcat(char *s1, char *s2, unsigned int n);
 void *_calloc(unsigned int nmemb, unsigned int size);
 int *array_range(int min, int max);
 int _putchar(char c);
+#ifndef _MAIN_H
+#define _MAIN_H
 #endif
